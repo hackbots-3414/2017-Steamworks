0bebdb1fde65df1c2e5dd3c579617116df1ac628
Kayleigh Francis <kayleighrf@gmail.com>
2017-01-15 21:51:55 -0500

    Added Accelerometer got rid of useless enum

--- src/org/usfirst/frc/team3414/robot/sensors/BuiltInAccelerometer.java
+++ src/org/usfirst/frc/team3414/robot/sensors/BuiltInAccelerometer.java
@@ -1,10 +1,43 @@
 package org.usfirst.frc.team3414.robot.sensors;
 
-public class BuiltInAccelerometer {
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class BuiltInAccelerometer implements IAccelerometer{
 
+	private Accelerometer accel;
+
+	public BuiltInAccelerometer(Accelerometer accel)
+	{
+		this.accel = accel;
 	}
 
+	@Override
+	public double getX()
+	{
+		return accel.getX();
+	}
+
+	@Override
+	public double getY()
+	{
+		return accel.getY();
+	}
+
+	@Override
+	public double getZ()
+	{
+		return accel.getZ();
+	}
+
+	@Override
+	public void reset()
+	{
+		// This does nothing
+	}
+
+	@Override
+	public double getResultant() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
--- src/org/usfirst/frc/team3414/robot/sensors/EButtonGamepad.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team3414.robot.sensors;
-
-public enum EButtonGamepad {
-
-}
